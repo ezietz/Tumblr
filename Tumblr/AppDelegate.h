c78860dbd65b7d16ed7f484ffe78afe59e4fb944
Elizabeth Zietz <ezietz@fb.com>
2019-06-27 10:29:13 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by ezietz on 6/27/19.
+//  Copyright © 2019 ezietz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
