a2ac5cf5f00a88bfc524c3f10aab817dd8ffa2bf
Elizabeth Zietz <ezietz@fb.com>
2019-06-27 12:12:45 -0700

    Finished app

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by ezietz on 6/27/19.
+//  Copyright © 2019 ezietz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+NS_ASSUME_NONNULL_END
